4f1deccfe9ebe403c41f3898ce69dcaa0ec1673a
Dustin Griffith <djarekg@gmail.com>
2025-11-05 18:33:50 -0500

    launch debug full stack seems to be working 🥳

--- apps/demo/next.config.ts
+++ apps/demo/next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // browserDebugInfoInTerminal: true,
+  cacheComponents: true,
+  reactCompiler: true,
+  typedRoutes: true,
+  experimental: {
+    // optimizePackageImports: [''], // https://nextjs.org/docs/app/api-reference/config/next-config-js/optimizePackageImports
+  },
 };
 
 export default nextConfig;
