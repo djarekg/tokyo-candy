81e0477f9ba0fa74f75914f6c6144bb9fe89b5fe
Dustin Griffith <djarekg@gmail.com>
2025-11-07 17:22:17 -0500

    updating to betterauth

--- apps/demo/src/lib/actions.ts
+++ apps/demo/src/lib/actions.ts
@@ -1,15 +1,25 @@
 'use server';
 
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
+import { signIn } from '@/lib/auth-client';
+import { APIError } from 'better-auth/api';
 
 export async function authenticate(_prevState: string | undefined, formData: FormData) {
   try {
-    await signIn('credentials', formData);
+    const email = formData.get('username')?.toString();
+    const password = formData.get('password')?.toString();
+
+    if (!email || !password) {
+      throw new APIError('BAD_REQUEST');
+    }
+
+    const {} = await signIn.email({
+      email,
+      password,
+    });
   } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
+    if (error instanceof APIError) {
+      switch (error.status) {
+        case 'BAD_REQUEST':
           return 'Invalid credentials.';
         default:
           return 'Something went wrong.';
---
+++ apps/demo/src/lib/auth-client.ts
@@ -0,0 +1,2 @@
+import { createAuthClient } from 'better-auth/react';
+export const { getSession, signIn, signOut, useSession } = createAuthClient();
